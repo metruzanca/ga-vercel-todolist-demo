0f97c361f8bb691eba0fb646fc9d8ce70eb14477
Sam Zanca <sam@zanca.dev>
2023-02-27 11:34:15 -0500

    other routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import todoRouter from './todoRouter.js'
 import listRouter from './listRouter.js'
+import authRouter from './authRouter.js'
 
 const apiRouter = Router()
 export default apiRouter
 
 apiRouter.use('/todo', todoRouter)
-// apiRouter.use('/list', listRouter)
-// apiRouter.use('/auth', authRouter)
+apiRouter.use('/list', listRouter)
+apiRouter.use('/auth', authRouter)
