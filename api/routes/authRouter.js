8f234b0c3a867f53985f335f5e92aac777703e63
Sam Zanca <sam@zanca.dev>
2023-02-27 11:18:03 -0500

    trying to get vercel 12 limit to work

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,9 +1,9 @@
-import { Router } from 'express'
-import * as controler from '../controllers/authController.js'
+// import { Router } from 'express'
+// import * as controler from '../controllers/authController.js'
 
-const router = Router()
-export default router
+// const router = Router()
+// export default router
 
-router.post('/login', controler.login)
-router.post('/register', controler.register)
-router.post('/changePassword', controler.changePassword)
+// router.post('/login', controler.login)
+// router.post('/register', controler.register)
+// router.post('/changePassword', controler.changePassword)
