f952b42349e350351c4f0267ffd77f0d6c13f192
Sam Zanca <sam@zanca.dev>
2023-02-27 11:06:03 -0500

    disabled auth routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,10 @@
 import { Router } from 'express'
 import todoRouter from './todoRouter.js'
 import listRouter from './listRouter.js'
-import authRouter from './authRouter.js'
 
 const apiRouter = Router()
 export default apiRouter
 
 apiRouter.use('/todo', todoRouter)
 apiRouter.use('/list', listRouter)
-apiRouter.use('/auth', authRouter)
+// apiRouter.use('/auth', authRouter)
